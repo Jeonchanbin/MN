2b83b33bbf5884c74feb44c224ba997673f923c8
Jeonchanbin <112598893+Jeonchanbin@users.noreply.github.com>
2022-11-30 10:20:51 +0900

    sdad

--- 06.React학습/react-app/my-react-app/src/index.js
+++ 06.React학습/react-app/my-react-app/src/index.js
@@ -8,3 +8,21 @@ import GAMES from "./DC/GAMES";
 import MOVIES from "./DC/MOVIES";
 import NEWS from "./DC/NEWS";
 import VIDEO from "./DC/VIDEO";
+
+/* 
+    [ 리액트 라우터 ]
+    -> 컴포넌트 연결하여 특정 이벤트에 모듈을 변경해주는 중계역할을 함 
+    1. <BrowserRouter> - 라우터 Root
+    2. <Routers> - 
+*/
+
+// 라우터를 이용한 컴포넌트 구성하기
+function App(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path=""
+            </Routes>
+        </BrowserRouter>
+    );
+};
