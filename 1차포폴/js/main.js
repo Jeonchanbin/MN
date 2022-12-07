b387ddababcfcd5b061d512462a5fab714975d93
Jeonchanbin <112598893+Jeonchanbin@users.noreply.github.com>
2022-12-07 17:07:36 +0900

    aaad

--- 1차포폴/js/main.js
+++ 1차포폴/js/main.js
@@ -10,9 +10,9 @@ $(()=>{
 })
 $(()=>{
     let gsts = 0;
-    $(".gnb1>a").click(function(e){
+    $(".gnb2 ul>li a").click(function(e){
         e.preventDefault();
-        $(".shopmenu>ol").animate({top:(gsts?"-80%":"5%")});
+        $(".smenu").animate({height:(gsts?"0%":"72%")});
         gsts?gsts=0:gsts=1;
 
     }); ////////// click /////////////
