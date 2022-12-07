60823bc93fc654063a92d7d722754486ed906328
Jeonchanbin <112598893+Jeonchanbin@users.noreply.github.com>
2022-12-07 10:19:42 +0900

    dfsf

--- 06.React학습/react-app/my-react-app/src/DC/Layout.js
+++ 06.React학습/react-app/my-react-app/src/DC/Layout.js
@@ -1,19 +1,20 @@
 import {Outlet, Link} from "react-router-dom";
 import Logo from "./Logo";
 import "./Layout.css";
+import "https://kit.fontawesome.com/a076d05399.js";
 /* 
     [ 리엑트 스타일링 기법 3가지 ]
+    1. 일반 CSS파일을 컴포넌트 JS파일에 import하여 사용
+    예) import "CSS경로";
 
-    1. 일반 CSS 파일을 컴포넌트 JS 파일에 import하여 사용
-    ex) import "CSS경로";
     2. 객체를 만들어서 적용하는 방법
-    예) 
-    const mystyle = {color: "red", fontSize:"30px"};
-    <h1 style={mystyle}>하하하<h1>
+    예)
+    const mystyle = {color:"red",fontSize:"30px"};
+    <h1 style={mystyle}>하하하</h1>
 
     3. 직접 중괄호 표현식에 중괄호를 하나 더 하여 표현
-    -> 중괄호 하나는 표현식, 안에 중괄호는 객체 (CSS용)
-    예) 
+    -> 중괄호 하나는 표현식, 안에 중괄호는 객체(CSS용)
+    예)
     <h1 style={{color:"red"}}>하하하</h1>
 */
 
@@ -48,6 +49,11 @@ const Layout = () => {
                     </li>
                     <li>
                         <Link to="/nw">NEWS</Link>
+                    </li>
+                    <li>
+                        <Link to="/mem">
+                            <i className='fas fa-users' style={{fontSize:"36px",color:"lime"}}></i>
+                        </Link>
                     </li>
                 </ul>
             </nav>
--- 06.React학습/react-app/my-react-app/src/index.js
+++ 06.React학습/react-app/my-react-app/src/index.js
@@ -8,30 +8,34 @@ import GAMES from "./DC/GAMES";
 import MOVIES from "./DC/MOVIES";
 import NEWS from "./DC/NEWS";
 import VIDEO from "./DC/VIDEO";
+import MEMBER from "./DC/MEMBER";
 
 /* 
     [ 리액트 라우터 ]
-    -> 컴포넌트 연결하여 특정 이벤트에 모듈을 변경해주는 중계역할을 함 
+    -> 컴포넌트 연결하여 특정 이벤트에 모듈을 변경해주는 중계역할을함
     1. <BrowserRouter> - 라우터 Root
-    2. <Routers> - 개별 라우터를 묶어주는 역할
+    2. <Routes> - 개별 라우터를 묶어주는 역할
     3. <Route> - 개별 라우터
-        속성 -> path: 경로를 지정함 (Link의 to속성 경로와 일치함)
-            -> element : 연결할 컴포넌트 지정
-        -> Root역할을 하는 <Route>를 가질 수 있다 -> Layout 컴포넌트
+        속성 -> path : 경로를 지정함(Link의 to속성 경로와 일치함!)
+            -> element : 연결할 컴포넌트지정
+        -> Root역할을 하는 <Route>는 
+        다른 하위 <Route>를 가질 수 있다! -> Layout 컴포넌트
         예) 
         <Route path="/">
+            <Route />
             <Route />
         </Route>
 */
 
-// 라우터를 이용한 컴포넌트 구성하기
+// 라우터를 이용한 컴포넌트 구성하기!
+// 컴포넌트 내보내기 해야함! export default
 export default function App(){
     return(
         <BrowserRouter>
             <Routes>
-                {/* 레이아웃 컴포넌트를 루트로 잡아줌 */}
+                {/* 레이아웃 컴포넌트를 루트로 잡아준다! */}
                 <Route path="/" element={<Layout />} >
-                    {/* path 대신 index만 쓰면 첫 페이지임 */}
+                    {/* path대신 index만 쓰면 첫페이지임! */}
                     <Route index element={<MOVIES />} />
                     <Route path="ct" element={<CHARACTERS />} />
                     <Route path="co" element={<COMICS />} />
@@ -39,13 +43,14 @@ export default function App(){
                     <Route path="mv" element={<MOVIES />} />
                     <Route path="nw" element={<NEWS />} />
                     <Route path="vd" element={<VIDEO />} />
+                    <Route path="mem" element={<MEMBER />} />
                 </Route>
             </Routes>
         </BrowserRouter>
     );
-};
+}
 
 
-// index.html의 div#root에 <App /> 컴포넌트 출력 지정하기
+// index.html의 div#root에 <App /> 컴포넌트 출력 지정하기!
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
